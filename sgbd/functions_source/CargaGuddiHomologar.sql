3e48e1628fdb8fd30b5e48241b816a9e6e35398e
thiagosoares <tfs.capanema@gmail.com>
2013-07-18 15:53:34 -0300

    complemento

--- sgbd/functions_source/CargaGuddiHomologar.sql
+++ sgbd/functions_source/CargaGuddiHomologar.sql
@@ -1,10 +1,8 @@
 
-
 INSERT INTO "guddi"."orgao" (id,nome,uf,codigo,email,endereco,nomecontato,razaosocial,sigla,url) VALUES (1,'SERPRO','DF','SERPRO','serpro@serpro.pa.gov.br','Rua 123','','SERVIÇO FEDERAL DE PROCESSAMENTO DE DADOS','SERPRO','http://www.serpro.gov.br');
 INSERT INTO "guddi"."orgao" (id,nome,uf,codigo,email,endereco,nomecontato,razaosocial,sigla,url) VALUES (2,'PRODEPA','PA','PRODEPA','prodepa@prodepa.pa.gov.br','Rodovia Augusto Montenegro km 10 · Centro Administrativo do Estado','CAP','PRODEPA','PRODEPA','http://www.prodepa.pa.gov.br/');
 INSERT INTO "guddi"."orgao" (id,nome,uf,codigo,email,endereco,nomecontato,razaosocial,sigla,url) VALUES (3,'TRT','PA','TRT','Umarizal - CEP 66050-100 - Belém-Pará-Amazônia-Brasil','','TRE','TRE','','http://www.trt8.jus.br/');
 INSERT INTO "guddi"."orgao" (id,nome,uf,codigo,email,endereco,nomecontato,razaosocial,sigla,url) VALUES (4,'TJE','PA','TJE','tje@tje.gov','Av. Almirante Barroso nº 3089 - Bairro: Souza - Belém - PA','','TJE','TJE','http://www.tjpa.jus.br');
-ALTER SEQUENCE guddi.orgao_id_seq RESTART WITH 100;
 
 
 INSERT INTO "guddi"."usuario" (id,nome,senha,email,id_orgao,papel,ativo,usuario,aminesia) VALUES (1,'System Admininstrator','e10adc3949ba59abbe56e057f20f883e','admin@guddi.org',1,4,true,'admin','');
@@ -15,7 +13,7 @@ INSERT INTO "guddi"."usuario" (id,nome,senha,email,id_orgao,papel,ativo,usuario,
 INSERT INTO "guddi"."usuario" (id,nome,senha,email,id_orgao,papel,ativo,usuario,aminesia) VALUES (7,'Teste de Manager','47f477','manager@manager.com',3,2,true,'manager2','a10f06547f79a8285b06047f47793c64');
 INSERT INTO "guddi"."usuario" (id,nome,senha,email,id_orgao,papel,ativo,usuario,aminesia) VALUES (8,'Paulo Gladson','a5924b','gladisonico@gladisonico.com',1,4,true,'gladisonico','068c519cab5d63b2665fda5924b02eb8');
 INSERT INTO "guddi"."usuario" (id,nome,senha,email,id_orgao,papel,ativo,usuario,aminesia) VALUES (10,'João','71606f','joao@joao.com',1,0,false,'joao','b662950a3b872e8e0126371606f41296');
-ALTER SEQUENCE guddi.usuario_id_seq RESTART WITH 100;
+
 
 INSERT INTO "guddi"."sistema" (id,nome,id_orgao,publico) VALUES (1,'Porto Sem Papel',1,true);
 INSERT INTO "guddi"."sistema" (id,nome,id_orgao,publico) VALUES (2,'Sistema Integrado de Administração de Serviços',1,true);
@@ -31,15 +29,14 @@ INSERT INTO "guddi"."sistema" (id,nome,id_orgao,publico) VALUES (12,'Push',3,tru
 INSERT INTO "guddi"."sistema" (id,nome,id_orgao,publico) VALUES (13,'Portal de Consultas',3,true);
 INSERT INTO "guddi"."sistema" (id,nome,id_orgao,publico) VALUES (14,'TJ Consultas',3,true);
 INSERT INTO "guddi"."sistema" (id,nome,id_orgao,publico) VALUES (15,'Sistema Sentinela',3,true);
-ALTER SEQUENCE guddi.sistema_id_seq RESTART WITH 100;
 
 INSERT INTO "guddi"."descritor" (id,url,id_sistema,tipo,nome,descricao) VALUES (1,'http://aplicacoes.mds.gov.br/teste/webservices/PPSPService',1,'WSDL','Porto Sem Papel','O PSP informatiza os procedimentos e integra, na medida em que promove a comunicação de dados entre os agentes intervenientes no processo portuário, eliminando o trâmite de 112 documentos, em diversas vias, e 935 informações em duplicidade junto às seis autoridades anuentes (Polícia Federal, Anvisa, Delegacia da Receita Federal, Vigiagro e Autoridade Portuária), preservando todos os seus aspectos inerentes ao sigilo e a segurança das informações nele produzidas. Isso irá reduzir em média 25% o tempo de estadia dos navios nos portos.');
 INSERT INTO "guddi"."descritor" (id,url,id_sistema,tipo,nome,descricao) VALUES (2,'http://200.120.41.331:9080/SIAS',2,'REST','SIAS','O Siasg é o sistema informatizado de apoio às atividades operacionais do Sistema de Serviços Gerais. A solução tem sido uma ferramenta importante para a modernização da área de serviços gerais na Administração Federal, em especial no cadastramento de fornecedores e de catálogo de materiais e serviços e no registro de preços de bens e serviços.Assim, o sistema é capaz de agilizar os processos de compra e promover a transparência dos atos do governo ao divulgar informações sobre os processos licitatórios. O sistema foi desenvolvido para o Ministério do Planejamento, Orçamento e Gestão (MPOG).');
 INSERT INTO "guddi"."descritor" (id,url,id_sistema,tipo,nome,descricao) VALUES (3,'http://aplicacoes.mds.gov.br/teste/webservices/PASService',3,'WSDL','CENTRAL SERVIÇOS','A Central de Serviços Serpro (CSS) é o canal de comunicação mais ágil entre os clientes da empresa ou usuários dos serviços Serpro com as equipes de especialistas. Confira as formas de abrir um acionamento na central e compreenda o seu funcionamento.');
-INSERT INTO "guddi"."descritor" (id,url,id_sistema,tipo,nome,descricao) VALUES (4,'http://sistemas.pa.gov.br/governodigital/ws/GovernoDigitalFacadeBeanWS?wsdl',4,'WSDL','Portal do Governo Digital','A partir de 1º de novembro de 2012 uma novidade vai transformar o modo de acesso de usuários da Prodepa: é o Governo Digital, um acesso único a todos os sistemas desenvolvidos pela empresa.E o melhor, é que nada mudou. Para acessar o Governo Digital basta entrar na página do sistema que você utiliza, com o mesmo login e senha para autenticar sua conta. As diversas aplicações ou serviços estarão disponíveis num só lugar.Para proporcionar a implantação do sistema único, a Prodepa realiza a migração dos demais sistemas. Nesse sentido, durante este dia serão feitos ainda alguns ajustes técnicos para a total excelência do serviço.');
+INSERT INTO "guddi"."descritor" (id,url,id_sistema,tipo,nome,descricao) VALUES (4,'http://10.1.11.14:8080/controleacesso/ws/ControleAcessoFacadeBeanWS?wsdl',4,'WSDL','Portal do Governo Digital','A partir de 1º de novembro de 2012 uma novidade vai transformar o modo de acesso de usuários da Prodepa: é o Governo Digital, um acesso único a todos os sistemas desenvolvidos pela empresa.E o melhor, é que nada mudou. Para acessar o Governo Digital basta entrar na página do sistema que você utiliza, com o mesmo login e senha para autenticar sua conta. As diversas aplicações ou serviços estarão disponíveis num só lugar.Para proporcionar a implantação do sistema único, a Prodepa realiza a migração dos demais sistemas. Nesse sentido, durante este dia serão feitos ainda alguns ajustes técnicos para a total excelência do serviço.');
 INSERT INTO "guddi"."descritor" (id,url,id_sistema,tipo,nome,descricao) VALUES (5,'http://10.1.11.14:8080/controleacesso/ws/ControleAcessoFacadeBeanWS?wsdl',5,'WSDL','Controle de Acesso','O sistema de controle de acesso controle asa permissões dos usuários em sistemas desenvolvidos pela prodepa');
 INSERT INTO "guddi"."descritor" (id,url,id_sistema,tipo,nome,descricao) VALUES (6,'http://10.1.11.14:8080/eprotocolo/ws/DocumentoFacadeBeanWS?wsdl',6,'WSDL','eProtocolo','O eprotocolo fornece controle digital sobre os documentos que tramitam no estado');
-ALTER SEQUENCE guddi.descritor_id_seq RESTART WITH 100;
+
 
 
 --Servicos 1-Porto Sem Papel 
@@ -77,7 +74,7 @@ INSERT INTO "guddi"."atributo" (id,descricao,nome,tipo,id_servico) VALUES (10,'I
 INSERT INTO "guddi"."excecao" (id,descricao,identificador,id_servico) VALUES (9,'Usuário não localizado','erro.usuario.01',4);
 INSERT INTO "guddi"."excecao" (id,descricao,identificador,id_servico) VALUES (10,'Usuário bloqueado','erro.usuario.02',4);
 
-INSERT INTO "guddi"."retorno" (id,descricao,tipo) VALUES (5,'Flag Indicativa da alteracao com sucesso','Boolean');
+INSERT INTO "guddi"."retorno" (id,descricao,tipo) VALUES (5,'Lista de usuarios encontrados','List<Usuario>');
 INSERT INTO "guddi"."servico" (id,id_descritor,nome,descricao,id_retorno) VALUES (5,4,'buscarUsuarios','Metodo para pesquisa de usuários',5);
 INSERT INTO "guddi"."atributo"(id,descricao,nome,tipo,id_servico) VALUES (11,'Nome, login ou documento do usuário','descricao','String',5);
 INSERT INTO "guddi"."excecao" (id,descricao,identificador,id_servico) VALUES (11,'Parâmetros inválidos','erro.pesquisa.03',5);
@@ -95,16 +92,11 @@ INSERT INTO "guddi"."excecao" (id,descricao,identificador,id_servico) VALUES (13
 
 INSERT INTO "guddi"."retorno" (id,descricao,tipo) VALUES (8,'Lista de tramitações de um protocolo','ArrayList<Tramitacao>');
 INSERT INTO "guddi"."servico" (id,id_descritor,nome,descricao,id_retorno) VALUES (8,6,'consultarTramitacoes','Metodo para obter a lista de tramitações de um documento',8);
-
 INSERT INTO "guddi"."atributo"(id,descricao,nome,tipo,id_servico) VALUES (14,'Identificador do protocolo','protocolo','String',8);
 INSERT INTO "guddi"."excecao" (id,descricao,identificador,id_servico) VALUES (14,'Protocolo não localizado','erro.protocolo.02',8);
 
 
-ALTER SEQUENCE guddi.servico_id_seq RESTART WITH 100;
-ALTER SEQUENCE guddi.atributo_id_seq RESTART WITH 100;
-ALTER SEQUENCE guddi.retorno_id_seq RESTART WITH 100;
-ALTER SEQUENCE guddi.excecao_id_seq RESTART WITH 100;
-ALTER SEQUENCE guddi.retorno_id_seq RESTART WITH 100;
+
 
 
 --Marcacoes
@@ -130,15 +122,41 @@ INSERT INTO "guddi"."marcacao" (id,marcacao) VALUES (16,'protocolo');
 INSERT INTO "guddi"."marcacao" (id,marcacao) VALUES (17,'Pará');
 INSERT INTO "guddi"."marcacao" (id,marcacao) VALUES (18,'Para');
 
-INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,2);
+
 INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,14);
 INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,15);
 INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,16);
 INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,16);
 INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,17);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (4,2);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (5,2);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,2);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (4,17);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (5,17);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,17);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (4,18);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (5,18);
+INSERT INTO "guddi"."descritor_marcacao" (id_descritor,id_marcacao) VALUES (6,18);
+
+
+
 ---continuar
 
 --delete from guddi.descritor_marcacao
 
 --delete from guddi.marcacao
+
+ALTER SEQUENCE guddi.servico_id_seq RESTART WITH 100;
+ALTER SEQUENCE guddi.atributo_id_seq RESTART WITH 100;
+ALTER SEQUENCE guddi.retorno_id_seq RESTART WITH 100;
+ALTER SEQUENCE guddi.excecao_id_seq RESTART WITH 100;
+ALTER SEQUENCE guddi.retorno_id_seq RESTART WITH 100;
+ALTER SEQUENCE guddi.descritor_id_seq RESTART WITH 100;
+ALTER SEQUENCE guddi.sistema_id_seq RESTART WITH 100;
+ALTER SEQUENCE guddi.usuario_id_seq RESTART WITH 100;
+ALTER SEQUENCE guddi.orgao_id_seq RESTART WITH 100;
+
+ALTER SEQUENCE guddi.marcacao_id_seq RESTART WITH 100;
+
+
 
